f3768255fd79b8ef64b5412c985da76a71c6253a
Marcelo Celano <104109443+MAC032@users.noreply.github.com>
2023-10-08 12:49:08 -0300

    Mensaje de Bienvenida

---
+++ gp1-01.js
@@ -0,0 +1,8 @@
+/*
+1) Realizá un programa que permita al usuario ingresar su nombre. La computadora debe emitir una salida con un mensaje de bienvenida que incluya el nombre ingresado.
+*/
+
+let nombre;
+nombre = prompt("Ingrese su nombre:");
+
+console.log("Te damos la bienvenida, " + nombre);
