1e24aca14a028c3914005e4d0c0ab1bac1bb3b0b
Marcelo Celano <104109443+MAC032@users.noreply.github.com>
2023-10-29 16:44:44 -0300

    Números del 1 al 100 y del 100 al 1

---
+++ gp3-01.js
@@ -0,0 +1,12 @@
+/*
+1) Realizá un programa que muestre todos los números enteros del 1 al 100, y luego, los mismos números, pero en orden inverso.
+*/
+
+console.log("Números de l al 100");
+for (let index = 1; index < 101; index++) {
+    console.log(index);   
+}
+console.log("Números de l00 al 1");
+for (let index = 100; index > 0; index--) {
+    console.log(index);   
+}
