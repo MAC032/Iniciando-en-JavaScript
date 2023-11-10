976a2fbc48c95a4e0986e70b196789eaac041641
Marcelo Celano <104109443+MAC032@users.noreply.github.com>
2023-11-10 17:28:33 -0300

    Validar nota con más excepciones while

--- gp3-11.js
+++ gp3-11.js
@@ -2,18 +2,21 @@
 11) Realizá un programa que permita validar una nota de examen de la misma manera que el   ejercicio anterior, pero con las siguientes nuevas directivas:
 ▪ La nota 0 se reserva para los ausentes.
 ▪ Las notas 1 y 3 no se suelen poner nunca.
+En resumen, las notas válidas deberán ser el 2 y el rango comprendido entre el 4 y el 10.
 */
 
-let nota;
+let nota = parseInt(prompt('Ingresa una nota'));
 
-nota = parseInt(prompt("Ingresa una nota"));
+while (nota !== 0 && nota !== 1 && nota !== 2 && nota !== 3 && (nota < 4 || nota > 10)) {
+  nota = parseInt(prompt('Ingresa una nota'));
+}
 
-while (nota > 10 || nota < 0) {
-    nota = parseInt(prompt("ERROR, nota fuera de rango"));
-}
-while (nota === 1 || nota === 3) {
-    nota = parseInt(prompt("ERROR, nota inválida"));
-}
-while (nota === 0) {
-    nota = parseInt(prompt("ERROR, nota reservada para los ausentes"));
+if (nota === 0) {
+  console.log('Esta ausente');
+} else if (nota === 1 || nota === 3) {
+    console.log('La nota no se suele poner nunca');
+} else if (nota === 2) {
+    console.log('La nota es válida');
+} else {
+    console.log('La nota es válida');
 }
