2918dd878e3d066000de3bf8e8aca89bb2aa945d
Marcelo Celano <104109443+MAC032@users.noreply.github.com>
2023-11-27 08:34:05 -0300

    Introducción a funciones

---
+++ 06-intro-funciones.js
@@ -0,0 +1,14 @@
+// Definición de funciones
+                 // Parámetro
+function siguiente (n) {
+    return n + 1;
+}
+
+function doble (n) {
+    return n * 2;
+}
+
+// Programa principal
+                             // Argumento
+let resultado = doble(siguiente(4));
+console.log(resultado);
