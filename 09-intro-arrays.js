ec9becb0cd5d0314362eaefe81fea691944e36e2
Marcelo Celano <104109443+MAC032@users.noreply.github.com>
2023-12-01 12:22:32 -0300

    Arrays

---
+++ 09-intro-arrays.js
@@ -0,0 +1,21 @@
+let colores = []
+
+console.log(colores.length)
+console.log(colores)
+
+colores.push("Rojo")
+colores.push("Verde")
+colores.push("Negro")
+colores.push("Azul")
+colores.push("Amarillo")
+colores.push("Naranja")
+colores.push("Blanco")
+
+console.log(colores.length)
+console.log(colores)
+
+for (let i = 0; i < colores.length; i++) {
+    const color = colores[i];
+    console.log(color.toLocaleUpperCase());
+    
+}
